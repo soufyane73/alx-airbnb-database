078ad456bd31348936be4172b9fd4fae2b2f8ef5
soufyane73 <82409191+soufyane73@users.noreply.github.com>
2025-07-06 22:01:39 +0100

    Create aggregations_and_window_functions.sql

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,27 @@
+-- Aggregations and Window Functions for ALX Airbnb Database Project
+
+-- Total number of bookings made by each user
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    Users u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name;
+
+-- Rank properties based on the total number of bookings
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM 
+    Property p
+LEFT JOIN 
+    Booking b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name;
