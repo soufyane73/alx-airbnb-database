a92b52babd6d70d8ee5852fd7fe947ecb0778159
soufyane73 <82409191+soufyane73@users.noreply.github.com>
2025-07-06 22:53:59 +0100

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -10,6 +10,5 @@ CREATE INDEX idx_booking_property_id ON Booking(property_id);
 -- Property table indexes
 CREATE INDEX idx_property_host_id ON Property(host_id);
 
-EXPLAIN SELECT * FROM Booking WHERE user_id = 'some_user_id';
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 'some_user_id';
 
-ANALYZE SELECT * FROM Booking WHERE user_id = 'some_user_id';
