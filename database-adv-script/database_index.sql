b3903c2a345f1a1eac95a8160028d9474faf3cf4
soufyane73 <82409191+soufyane73@users.noreply.github.com>
2025-07-06 22:06:30 +0100

    Create database_index.sql

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,11 @@
+-- Create indexes for optimization
+
+-- User table indexes
+CREATE INDEX idx_users_email ON Users(email);
+
+-- Booking table indexes
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+
+-- Property table indexes
+CREATE INDEX idx_property_host_id ON Property(host_id);
