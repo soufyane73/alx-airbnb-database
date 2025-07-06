530e8956369e9e80bde7b4f209292a2fcade483a
soufyane73 <82409191+soufyane73@users.noreply.github.com>
2025-07-06 22:51:14 +0100

    Create performance.sql

---
+++ database-adv-script/performance.sql
@@ -0,0 +1,42 @@
+-- Initial Query to Retrieve All Bookings with User, Property, and Payment Details
+
+SELECT 
+    b.booking_id,
+    b.user_id,
+    b.property_id,
+    u.first_name AS user_first_name,
+    u.last_name AS user_last_name,
+    p.name AS property_name,
+    p.location AS property_location,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date
+FROM 
+    Booking b
+JOIN 
+    Users u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+JOIN 
+    Payment pay ON b.booking_id = pay.booking_id;
+
+
+EXPLAIN SELECT 
+    b.booking_id,
+    b.user_id,
+    b.property_id,
+    u.first_name AS user_first_name,
+    u.last_name AS user_last_name,
+    p.name AS property_name,
+    p.location AS property_location,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date
+FROM 
+    Booking b
+JOIN 
+    Users u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+JOIN 
+    Payment pay ON b.booking_id = pay.booking_id;
